8f10cc22c8aabfc0e6a0d08102da963709ee5e91
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-01-06 20:18:19 +0400

    actuator endpoints was secured

--- src/main/java/az/maqa/spring/ActuatorSecurityConfig.java
+++ src/main/java/az/maqa/spring/ActuatorSecurityConfig.java
@@ -1,17 +1,19 @@
 package az.maqa.spring;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class ActuatorSecurityConfig {
+public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
 
-   /* @Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        *//*http
+        http
                 .authorizeRequests()
 
                 .antMatchers("/actuator/**").hasAuthority("ACTUATOR_ADMIN")
 
-                .anyRequest().authenticated();*//*
-    }*/
+                .anyRequest().authenticated();
+    }
 }
